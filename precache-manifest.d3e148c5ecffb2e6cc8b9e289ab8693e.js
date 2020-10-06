b61bfb588e7a52f095b76685bfa064e305023391
Joshua Patterson <joshkpatt@gmail.com>
2020-10-06 14:58:56 -0400

    Updates

---
+++ precache-manifest.d3e148c5ecffb2e6cc8b9e289ab8693e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5ef862447a86dbba936",
+    "url": "/react-todos-class-based/static/js/main.69c2ac31.chunk.js"
+  },
+  {
+    "revision": "6197e9874f05a6fae941",
+    "url": "/react-todos-class-based/static/js/runtime~main.2000f606.js"
+  },
+  {
+    "revision": "e6d9cade2f2632aad926",
+    "url": "/react-todos-class-based/static/js/2.4f3b3be1.chunk.js"
+  },
+  {
+    "revision": "f4f3cb2ae908cff0fcf5d755bd6c3392",
+    "url": "/react-todos-class-based/index.html"
+  }
+];
